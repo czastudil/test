b7f569c1a82f7726598eaf2139115a54eb1d9e7c
zastudcr <zastudcr@dukes.jmu.edu>
2024-07-15 14:30:48 -0400

    add code to main

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import threading
 import shutil
 import random
 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from main import *
+
+import time
+import random
+import json
+
 alpha_value = 77
 colors = [
     [[255, 182, 193, alpha_value], [255, 105, 180, alpha_value]],  # Light Pink -> Hot Pink
@@ -21,7 +29,55 @@ colors = [
     [[255, 250, 205, alpha_value], [255, 255, 0, alpha_value]],     # Lemon Chiffon -> Yellow
 ]
 
+def write_image(fileName, image_data):
+    if image_data.startswith('data:image/png;base64,'):
+        image_data = image_data.replace('data:image/png;base64,', '')
+    
+    # Decode the base64 string
+    decoded_data = base64.b64decode(image_data)
+    
+    # Convert the byte data to a numpy array
+    np_arr = np.frombuffer(decoded_data, np.uint8)
+    
+    # Decode the numpy array to an image
+    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    
+    # Save the image
+    cv2.imwrite(fileName, image)
 
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+user_img_url = "./userImage.png"
+
+@app.route('/api/send-data', methods=['POST'])
+def receive_data():
+    image_data = request.json  # Assuming JSON data is sent
+    write_image(user_img_url, image_data)
+    
+    hotspots = retrieve_data(user_img_url)
+    for hs in hotspots:
+        print (hs.toJSON())
+
+    return jsonify([hs.toJSON() for hs in hotspots])
+    
+
+@app.route('/api/send-mask', methods=['POST'])
+def receive_mask():
+    image_data = request.json
+    write_image(user_img_url, image_data)
+    return
+
+@app.route('/api/send-VSD', methods=['POST'])
+def receive_VSD():
+    with open("sampleVSD.json", "w") as VSD:
+        json.dump(request.json, VSD)
 
 class Hotspot:
     def __init__(self, hotspotName='', options=[]):
@@ -173,6 +229,9 @@ def retrieve_data(user_img_url):
     hotspots = parse_hotspots(conversation.conversation[-1].text)
     return hotspots
                           
+
+if __name__ == "__main__":
+    app.run()
 
 '''
 if __name__ == "__main__":
